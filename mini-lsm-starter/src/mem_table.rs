29a503a7ca645e58848a69a042b50a1d01583169
Xiang Yu Tuang <xiangyutuang@gmail.com>
2025-05-23 18:21:51 -0500

    create,get,put

--- mini-lsm-starter/src/mem_table.rs
+++ mini-lsm-starter/src/mem_table.rs
@@ -53,7 +53,12 @@ pub(crate) fn map_bound(bound: Bound<&[u8]>) -> Bound<Bytes> {
 impl MemTable {
     /// Create a new mem-table.
     pub fn create(_id: usize) -> Self {
-        unimplemented!()
+        MemTable {
+            map: Arc::new(SkipMap::new()),
+            wal: None,
+            id: _id,
+            approximate_size: Arc::new(AtomicUsize::new(0)),
+        }
     }
 
     /// Create a new mem-table with WAL
@@ -87,7 +92,13 @@ impl MemTable {
 
     /// Get a value by key.
     pub fn get(&self, _key: &[u8]) -> Option<Bytes> {
-        unimplemented!()
+        let cloned_map = Arc::clone(&self.map);
+        let opt_value = cloned_map.get(_key);
+
+        match opt_value {
+            None => None,
+            Some(entry) => Some(entry.value().clone()),
+        }
     }
 
     /// Put a key-value pair into the mem-table.
@@ -96,7 +107,9 @@ impl MemTable {
     /// In week 2, day 6, also flush the data to WAL.
     /// In week 3, day 5, modify the function to use the batch API.
     pub fn put(&self, _key: &[u8], _value: &[u8]) -> Result<()> {
-        unimplemented!()
+        let cloned_map = Arc::clone(&self.map);
+        cloned_map.insert(Bytes::copy_from_slice(_key), Bytes::copy_from_slice(_value));
+        Ok(())
     }
 
     /// Implement this in week 3, day 5.
